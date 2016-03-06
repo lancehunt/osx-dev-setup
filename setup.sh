8396bbf0399e69220137c9ec3723b1706af1c672
Lance Hunt <lhunt@RIENGLM013.trad.tradestation.com>
2016-03-06 02:24:29 -0600

    First stab at an osx Setup script from scratch

---
+++ setup.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# Install Xcode command line tools.
+xcode-select --install
+
+# install HomeBrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install brew-cask
+brew update && brew cleanup && brew cask cleanup
+
+# Configure Homebrew
+brew doctor
+
+# install cask
+brew install cask
+
+# *** BEGIN Configuring ***
+
+# install Config Manager (for dotfiles)
+brew install vcsh
+
+# install Multiple Repository management tool
+brew install mr
+
+# *** SETUP DOTFILES 
+
+
+# *** BEGIN Apps ***
+brew cask install iterm2
+brew cask install dropbox 
+brew cask install slack
+brew cask install gitter
+brew cask install spectacle
+brew cask install 1password
+brew cask install google-chrome
+
+# *** BEGIN SDK's & Runtimes ***
+# install Go-lang
+brew cask install go
+export $GOPATH=$HOME/go
+# install Node Version Manager (NVM)
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+# install Docker Toolbox
+brew cask install dockertoolbox
+
+# *** BEGIN Dev Tools ***
+# install Ack (grep for code) tool
+brew install ack
+# Update VIM to 7.4
+brew reinstall vim --with-lua
+# fix vimrc
+cp /usr/local/share/vim/vim74/vimrc_example.vim ~/.vimrc
+echo "set clipboard=unnamed" >> ~./.vimrc
+# Install VSCode
+brew cask install visual-studio-code
+# Misc other tools
+brew install openssh
+brew cask install imageoptim
+
+
+# *** BEGIN Settings ***
+#Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 0.02
+#Set a shorter Delay until key repeat
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
+#Show the ~/Library folder
+chflags nohidden ~/Library
+#Store screenshots in subfolder on desktop
+mkdir ~/Desktop/Screenshots
+defaults write com.apple.screencapture location ~/Desktop/Screenshots
+
+# Enable "Tap to Click".
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Invert scrolling direction.
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+# Open Finder at "~/" directory.
+defaults write com.apple.finder NewWindowTarget -string "PfLo"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
+
+# Setup Git.
+read -e -p "Enter your git username: " USERNAME
+read -e -p "Enter your git email address: " EMAIL
+git config --global user.name $USERNAME
+git config --global user.email $EMAIL
+ssh-keygen -t rsa -b 4096 -C $EMAIL
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+pbcopy < ~/.ssh/id_rsa.pub
+
+
