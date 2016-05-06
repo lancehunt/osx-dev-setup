9c4d4b64e0d087af30ca2028dd0ab183f68a824b
Lance Hunt <lancehunt@gmail.com>
2016-05-06 10:58:10 -0500

    Add AWS cli setup & update settings

--- setup/aws.sh
+++ setup/aws.sh
@@ -2,4 +2,9 @@
 
 echo "Installing AWS tools"
 
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+rm awscli-bundle.zip
+
 # TODO
